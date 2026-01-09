28effc2735b637e9936796fce27578d11e0ee491
MohanaStuio <psdcreative17@gmail.com>
2026-01-09 18:05:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,17 @@
+import "./globals.css"
 import { Providers } from "./providers"
+import type { ReactNode } from "react"
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
